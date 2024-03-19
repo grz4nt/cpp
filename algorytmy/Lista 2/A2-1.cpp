214857575085ad2062eeb5bbb16ab77c16d29128
grz4nt <grz4nt@gmail.com>
2024-03-19 20:20:08 +0100

    wtorek

---
+++ algorytmy/Lista 2/A2-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const int rozmiar = 10;
+
+void element(int a[], int i = 0) {
+    if (i < rozmiar) {
+        cout << "a[" << i << "]: " << *(&a[0]+i) << endl;
+        element(a, i+1);
+    }
+}
+
+int main () {
+    srand(time(nullptr));
+    int a[rozmiar];
+    cout << "Losowe wartosci w tablicy:\n";
+    for (int i = 0; i < rozmiar; i++) {
+        a[i] = rand()%100 + 1;
+        cout << i << ": " << a[i] << endl;
+    }
+    cout << "Odwolania do wartosci w tablicy:" << endl;
+    element(a);
+    return 0;
+}
