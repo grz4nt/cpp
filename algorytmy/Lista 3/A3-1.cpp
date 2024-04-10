d4bcc60970e0045298852114871054c9d40704fb
grz4nt <grz4nt@gmail.com>
2024-04-10 20:08:00 +0200

    sroda

--- algorytmy/Lista 3/A3-1.cpp
+++ algorytmy/Lista 3/A3-1.cpp
@@ -1,8 +1,172 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-using namespace std;
+struct lista
+{
+    std::string imie;
+    std::string nazwisko;
+    std::string PESEL;
+    int wiek;
+    lista *pop;
+};
 
-int main() {
+lista *dodajElement(lista *pocz, std::string imie, std::string nazwisko, std::string PESEL, int wiek)
+{
+    lista *nowyElement = new lista;
+    nowyElement->imie = imie;
+    nowyElement->nazwisko = nazwisko;
+    nowyElement->PESEL = PESEL;
+    nowyElement->wiek = wiek;
+    nowyElement->pop = pocz;
+    return nowyElement;
+}
+
+lista *zdejmijElement(lista *pocz)
+{
+    if (pocz)
+    {
+        lista *temp = pocz;
+        pocz = pocz->pop;
+        std::cout << "Usuniety element: " << temp->imie << " " << temp->nazwisko << ", PESEL: " << temp->PESEL << ", wiek: " << temp->wiek << "\n";
+        delete temp;
+    }
+    return pocz;
+}
+
+void wypiszStos(lista *pocz)
+{
+    lista *temp = pocz;
+    while (temp)
+    {
+        std::cout << "Imie i nazwisko: " << temp->imie << " " << temp->nazwisko << ", PESEL: " << temp->PESEL << ", wiek: " << temp->wiek << "\n";
+        temp = temp->pop;
+    }
+}
+
+void zapiszDoPliku(lista *pocz, const std::string &nazwaPliku)
+{
+    std::ofstream plik(nazwaPliku);
+    if (plik.is_open())
+    {
+        lista *temp = pocz;
+        while (temp)
+        {
+            plik << temp->imie << " " << temp->nazwisko << ", " << temp->PESEL << ", " << temp->wiek << " lat\n";
+            temp = temp->pop;
+        }
+        plik.close();
+        std::cout << "Zapis do pliku " << nazwaPliku << " powiodl sie." << std::endl;
+    }
+    else
+    {
+        std::cout << "Nie udalo sie otworzyc pliku " << nazwaPliku << " do zapisu." << std::endl;
+    }
+}
+
+void wyswietlMenu()
+{
+    std::cout << "a. Dodaj element\n";
+    std::cout << "b. Zdejmij element\n";
+    std::cout << "c. Pokaz zawartosc stosu\n";
+    std::cout << "d. Zapisz dane do pliku\n";
+    std::cout << "f. Zakoncz program\n";
+    std::cout << "Wybierz funkcje: ";
+}
+
+bool sprawdzStos(lista *pocz)
+{
+    if (pocz == nullptr)
+    {
+        std::cout << "Nie mozna wykonac wybranej funkcji. Stos jest pusty!\n" << std::endl;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    lista *pocz = nullptr;
+    char wybor;
+    std::string imie, nazwisko, PESEL;
+    int wiek;
+
+    do
+    {
+        wyswietlMenu();
+        std::cin >> wybor;
+        switch (wybor)
+        {
+        case 'a':
+            bool valid;
+            std::cout << "Imie: ";
+            std::cin.ignore();
+            std::getline(std::cin, imie);
+            std::cout << "Nazwisko: ";
+            std::getline(std::cin, nazwisko);
+            std::cout << "PESEL: ";
+            do
+            {
+                std::cin >> PESEL;
+                valid = true;
+                for (char c : PESEL)
+                {
+                    if (c < '0' || c > '9')
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (PESEL.length() != 11 || !valid)
+                {
+                    std::cout << "PESEL musi skladac sie z 11 cyfr. Wprowadz ponownie: ";
+                }
+            } while (PESEL.length() != 11 || !valid);
+            std::cout << "Wiek: ";
+            do
+            {
+                std::cin >> wiek;
+                if (wiek < 1 || wiek > 125)
+                {
+                    std::cout << "Wprowadz ponownie: ";
+                }
+            } while (wiek < 1 || wiek > 125);
+            pocz = dodajElement(pocz, imie, nazwisko, PESEL, wiek);
+            break;
+            std::cout << std::endl;
+        case 'b':
+            if (!sprawdzStos(pocz))
+            {
+                pocz = zdejmijElement(pocz);
+            }
+            std::cout << std::endl;
+            break;
+        case 'c':
+            if (!sprawdzStos(pocz))
+            {
+                wypiszStos(pocz);
+            }
+            std::cout << std::endl;
+            break;
+        case 'd':
+            if (!sprawdzStos(pocz))
+            {
+                zapiszDoPliku(pocz, "dane.txt");
+                std::cout << "Dane zapisane do pliku dane.txt\n" << std::endl;
+            }
+            break;
+        case 'f':
+            while (pocz)
+            {
+                pocz = zdejmijElement(pocz);
+            }
+            std::cout << "Program zakonczony.";
+            break;
+        default:
+            std::cout << "Nieznana opcja\n";
+            break;
+        }
+    } while (wybor != 'f');
 
     return 0;
 }
---
+++ algorytmy/Lista 3/A3-2.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct lista
+{
+    string imie;
+    string nazwisko;
+    string PESEL;
+    int wiek;
+    lista *nast;
+};
+
+lista *pocz = nullptr, *kon = nullptr;
+
+void dodaj(string imie, string nazwisko, string PESEL, int wiek)
+{
+    lista *nowy = new lista;
+    nowy->imie = imie;
+    nowy->nazwisko = nazwisko;
+    nowy->PESEL = PESEL;
+    nowy->wiek = wiek;
+    nowy->nast = nullptr;
+
+    if (kon != nullptr)
+        kon->nast = nowy;
+    else
+        pocz = nowy;
+
+    kon = nowy;
+}
+
+void usun()
+{
+    if (pocz != nullptr)
+    {
+        lista *temp = pocz;
+        pocz = pocz->nast;
+        delete temp;
+
+        if (pocz == nullptr)
+            kon = nullptr;
+    }
+}
+
+void wypisz()
+{
+    lista *temp = pocz;
+    while (temp != nullptr)
+    {
+        cout << "Imie i nazwisko: " << temp->imie << " " << temp->nazwisko << ", PESEL: " << temp->PESEL << ", wiek: " << temp->wiek << endl;
+        temp = temp->nast;
+    }
+}
+
+void usunWszystko()
+{
+    while (pocz != nullptr)
+        usun();
+}
+
+void zapiszDoPliku(string nazwaPliku)
+{
+    vector<lista *> wektor;
+    lista *temp = pocz;
+    while (temp != nullptr)
+    {
+        wektor.push_back(temp);
+        temp = temp->nast;
+    }
+
+    sort(wektor.begin(), wektor.end(), [](lista *a, lista *b)
+         { return a->wiek < b->wiek; });
+
+    ofstream plik(nazwaPliku);
+    if (plik.is_open())
+    {
+        for (lista *element : wektor)
+            plik << element->imie << " " << element->nazwisko << ", " << element->PESEL << ", " << element->wiek << " lat\n";
+        plik.close();
+        cout << "Zapis do pliku " << nazwaPliku << " powiodl sie." << endl;
+    }
+    else
+    {
+        cout << "Nie udalo sie otworzyc pliku " << nazwaPliku << " do zapisu." << endl;
+    }
+}
+
+bool sprawdzKolejke()
+{
+    if (pocz == nullptr)
+    {
+        cout << "Nie mozna wykonac operacji. Lista jest pusta!\n";
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    char wybor;
+    string imie, nazwisko, PESEL;
+    int wiek;
+    dodaj("Jan Maria", "Rokita A", "12345678901", 11);
+    dodaj("Anna", "Nowak", "23456789012", 33);
+    dodaj("Piotr", "Zielinski", "34567890123", 22);
+    dodaj("Katarzyna", "Wisniewska", "45678901234", 99);
+    dodaj("Kasia", "Babs", "46346671234", 177);
+    dodaj("Pioter", "Pjoter", "32220123", 7);
+    do
+    {
+        cout << "a) Dodaj element na koniec kolejki\n"
+             << "b) Usun element z poczatku kolejki\n"
+             << "c) Pokaz zawartosc kolejki\n"
+             << "d) Usun wszystkie elementy kolejki\n"
+             << "e) Zapisz elementy do pliku (sortowanie wg wieku)\n"
+             << "f) Zakoncz program\n"
+             << "Wybierz opcje: ";
+        cin >> wybor;
+
+        switch (wybor)
+        {
+        case 'a':
+            bool valid;
+            cout << "Imie: ";
+            cin.ignore();
+            getline(cin, imie);
+            cout << "Nazwisko: ";
+            getline(cin, nazwisko);
+            cout << "PESEL: ";
+            do
+            {
+                cin >> PESEL;
+                valid = true;
+                for (char c : PESEL)
+                {
+                    if (c < '0' || c > '9')
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (PESEL.length() != 11 || !valid)
+                {
+                    cout << "PESEL musi skladac sie z 11 cyfr. Wprowadz ponownie: ";
+                }
+            } while (PESEL.length() != 11 || !valid);
+            cout << "Wiek: ";
+            do
+            {
+                cin >> wiek;
+                if (wiek < 1 || wiek > 125)
+                {
+                    cout << "Wprowadz ponownie: ";
+                }
+            } while (wiek < 1 || wiek > 125);
+            dodaj(imie, nazwisko, PESEL, wiek);
+            cout << "Dodano element na koniec kolejki.\n" << endl;
+            break;
+        case 'b':
+            if (sprawdzKolejke())
+                usun();
+            cout << "Usunieto element z kolejki.\n" << endl;
+            break;
+        case 'c':
+            if (sprawdzKolejke())
+                wypisz();
+            cout << endl;
+            break;
+        case 'd':
+            if (sprawdzKolejke())
+                usunWszystko();
+            cout << endl;
+            break;
+        case 'e':
+            if (sprawdzKolejke())
+                zapiszDoPliku("kolejka.txt");
+            cout << endl;
+            break;
+        case 'f':
+            usunWszystko();
+            break;
+        default:
+            cout << "Nieznana opcja." << endl;
+        }
+    } while (wybor != 'f');
+
+    return 0;
+}
