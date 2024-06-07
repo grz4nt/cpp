e76ecaf4c039d5f1ecce59464712073fab436f62
grz4nt <grz4nt@gmail.com>
2024-06-07 20:28:17 +0200

    piatek

--- prog/Lista8/8.cpp
+++ prog/Lista8/8.cpp
@@ -22,11 +22,13 @@ public:
     }
 
     friend ostream& operator<<(ostream& os, const Ocena& o)
-    {
-        os << "Przedmiot: " << o.przedmiot << " (" << o.imie_p << " " << o.nazwisko_p << ")" << endl;
-        os << "Ocena: " << o.ocena << " (" << o.data << ")" << endl;
-        return os;
-    }
+{
+    os << o.przedmiot << " (" << o.imie_p << " " << o.nazwisko_p << ")\n";
+    os << "Ocena: " << o.ocena << " (" << o.data << ")\n";
+    return os;
+}
+
+
 };
 
 class Semestr
@@ -42,15 +44,17 @@ public:
         oceny.push_back(ocena);
     }
 
-    friend ostream& operator<<(ostream& os, const Semestr& s)
+   friend ostream& operator<<(ostream& os, const Semestr& s)
+{
+    os << s.numer << " semestr:\n";
+    for (const auto& ocena : s.oceny)
     {
-        os << "Semestr: " << s.numer << endl;
-        for (const auto& ocena : s.oceny)
-        {
-            os << ocena << endl;
-        }
-        return os;
+        os << ocena;
     }
+    return os;
+}
+
+
 };
 
 class Student
@@ -73,17 +77,17 @@ public:
     }
 
     friend ostream& operator<<(ostream& os, const Student& s)
+{
+    os << s.imie << " " << s.nazwisko << " (" << s.album << ")\n";
+    os << "Aktualny semestr: " << s.aktSem << "\n";
+    for (const auto& sem : s.historiaSemestrow)
     {
-        os << s.imie << " " << s.nazwisko << " (" << s.album << ")" << endl;
-        os << "Aktualny semestr: " << s.aktSem << endl;
-        os << "Historia semestrow: " << endl;
-        for (const auto& sem : s.historiaSemestrow)
-        {
-            os << sem << std::endl;
-        }
-        os << "Srednia ocen: " << s.avg << endl;
-        return os;
+        os << sem;
     }
+    os << "Srednia ocen: " << s.avg << "\n-------------------\n";
+    return os;
+}
+
 
     void edytujDane()
     {
@@ -107,7 +111,7 @@ public:
         cin >> student.album;
         cout << "Srednia ocen: ";
         cin >> student.avg;
-        cout << endl;
+        cout << "\n";
 
         student.historiaSemestrow.clear();
         for (int i = 1; i <= student.aktSem; ++i)
@@ -170,18 +174,18 @@ void listaStudentow()
 
 void del()
 {
-    cout << "Podaj nr albumu studenta do usuniecia: " << endl;
+    cout << "Podaj nr albumu studenta do usuniecia: \n";
     int del;
     cin >> del;
     auto it = find_if(student.begin(), student.end(), [&](const Student& s) { return s.album == del; });
     if (it != student.end())
     {
         student.erase(it);
-        cout << "Student zostal usuniety z listy" << endl;
+        cout << "Student zostal usuniety z listy \n";
     }
     else
     {
-        cout << "Nie znaleziono studenta o podanym albumie" << endl;
+        cout << "Nie znaleziono studenta o podanym albumie \n";
     }
 }
 
@@ -219,7 +223,7 @@ void ocena()
     }
     else
     {
-        cout << "Nie znaleziono studenta o podanym albumie" << endl;
+        cout << "Nie znaleziono studenta o podanym albumie\n";
     }
 }
 
@@ -233,11 +237,11 @@ void znaj()
 
     if (it != end(student))
     {
-        cout << "Wynik wyszukiwania: " << *it << endl;
+        cout << "Wynik wyszukiwania: " << *it << "\n";
     }
     else
     {
-        cout << "Nie znaleziono studenta o nazwisku " << nazwisko << endl;
+        cout << "Nie znaleziono studenta o nazwisku " << nazwisko << "\n";
     }
 }
 
@@ -252,13 +256,13 @@ void edit()
     {
         Student* s = &(*it);
         int w;
-        cout << "Wybierz dane do edycji:" << endl
-             << " 1 Imie" << endl
-             << " 2 Nazwisko" << endl
-             << " 3 Aktualny semestr" << endl
-             << " 4 Nr albumu" << endl
-             << " 5 Wszystkie pola" << endl
-             << "Wybor: " << endl;
+        cout << "Wybierz dane do edycji:\n"
+             << " 1 Imie\n"
+             << " 2 Nazwisko\n"
+             << " 3 Aktualny semestr\n"
+             << " 4 Nr albumu\n"
+             << " 5 Wszystkie pola\n"
+             << "Wybor: ";
         cin >> w;
         switch (w)
         {
@@ -292,13 +296,13 @@ void edit()
             s->edytujDane();
             break;
         default:
-            cout << "Niepoprawny wybor." << endl;
+            cout << "Niepoprawny wybor.\n";
             break;
         }
     }
     else
     {
-        cout << "Nie znaleziono studenta o podanym albumie" << endl;
+        cout << "Nie znaleziono studenta o podanym albumie\n";
     }
 }
 
@@ -307,17 +311,51 @@ int main()
 {
     int w;
     bool exit = false;
+    student.push_back(Student("Anna", "Kowalska", 3, 12345, 4.5));
+    student[0].dodajOcene(1, Ocena("Matematyka", "Jan", "Nowak", 5, "2023-01-15"));
+    student[0].dodajOcene(2, Ocena("Fizyka", "Maria", "Wisniewska", 4, "2023-06-10"));
+    student[0].dodajOcene(1, Ocena("Informatyka", "Piotr", "Kowalczyk", 3, "2021-12-05"));
+    student[0].dodajOcene(2, Ocena("Informatyka", "Piotr", "Kowalczyk", 4, "2022-12-05"));
+    student[0].dodajOcene(3, Ocena("Informatyka", "Piotr", "Kowalczyk", 5, "2023-12-05"));
+
+    student.push_back(Student("Bartlomiej", "Wisniewski", 2, 54321, 3.8));
+    student[1].dodajOcene(1, Ocena("Matematyka", "Jan", "Nowak", 4, "2023-01-15"));
+    student[1].dodajOcene(2, Ocena("Chemia", "Katarzyna", "Zielinska", 3, "2023-06-10"));
+
+    student.push_back(Student("Celina", "Nowak", 4, 13579, 4.2));
+    student[2].dodajOcene(1, Ocena("Biologia", "Adam", "Sikorski", 4, "2023-01-15"));
+    student[2].dodajOcene(2, Ocena("Geografia", "Ewa", "Kowalska", 5, "2023-06-10"));
+    student[2].dodajOcene(3, Ocena("Historia", "Tomasz", "Lewandowski", 4, "2023-12-05"));
+    student[2].dodajOcene(4, Ocena("Fizyka", "Maria", "Wisniewska", 4, "2024-01-20"));
+
+    student.push_back(Student("Dawid", "Zielinski", 1, 102938, 4.0));
+    student[3].dodajOcene(1, Ocena("Matematyka", "Jan", "Nowak", 4, "2023-01-15"));
+
+    student.push_back(Student("Ewa", "Lewandowska", 3, 102030, 3.9));
+    student[4].dodajOcene(1, Ocena("Informatyka", "Piotr", "Kowalczyk", 4, "2023-01-15"));
+    student[4].dodajOcene(2, Ocena("Matematyka", "Jan", "Nowak", 3, "2023-06-10"));
+    student[4].dodajOcene(3, Ocena("Chemia", "Katarzyna", "Zielinska", 5, "2023-12-05"));
+
+    student.push_back(Student("Filip", "Wojciechowski", 2, 101112, 3.5));
+    student[5].dodajOcene(1, Ocena("Geografia", "Ewa", "Kowalska", 4, "2023-01-15"));
+    student[5].dodajOcene(2, Ocena("Historia", "Tomasz", "Lewandowski", 3, "2023-06-10"));
+
+    student.push_back(Student("Gabriela", "Kaminska", 2, 112233, 4.1));
+    student[6].dodajOcene(1, Ocena("Biologia", "Adam", "Sikorski", 5, "2023-01-15"));
+    student[6].dodajOcene(1, Ocena("Fizyka", "Maria", "Wisniewska", 3, "2022-06-10"));
+    student[6].dodajOcene(2, Ocena("Biologia", "Adam", "Sikorski", 5, "2024-12-15"));
+    student[6].dodajOcene(2, Ocena("Fizyka", "Maria", "Wisniewska", 3, "2023-06-10"));
     while (!exit)
     {
-        cout << "1. Dodanie studenta" << endl;
-        cout << "2. Usuniecie studenta" << endl;
-        cout << "3. Edytowanie studenta" << endl;
-        cout << "4. Przeglad studentow" << endl;
-        cout << "5. Wyszukaj studenta" << endl;
-        cout << "6. Wpisz ocene" << endl;
-        cout << "7. Zakoncz" << endl;
+        cout << "1. Dodanie studenta\n";
+        cout << "2. Usuniecie studenta\n";
+        cout << "3. Edytowanie studenta\n";
+        cout << "4. Przeglad studentow\n";
+        cout << "5. Wyszukaj studenta\n";
+        cout << "6. Wpisz ocene\n";
+        cout << "7. Zakoncz\n";
         cin >> w;
-        cout << "---------------------" << endl;
+        cout << "---------------------\n";
 
         switch (w)
         {
@@ -343,7 +381,7 @@ int main()
             exit = true;
             break;
         default:
-            cout << "Niepoprawny wybor, sprobuj ponownie." << endl;
+            cout << "Niepoprawny wybor, sprobuj ponownie.\n";
             break;
         }
     }
